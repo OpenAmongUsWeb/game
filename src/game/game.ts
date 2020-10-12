2d60838891ec0ca4f8688868e4896bb344b26f07
Mareps <mareh.ps@gmail.com>
2020-10-12 12:50:50 -0300

    adding main game class

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,13 +1,39 @@
 import { Canvas } from "../canvas/canvas.js";
 import { Player } from "../player/player.js";
 
-window.addEventListener('load', () => loadGame());
+export class Game {
 
-function loadGame(): void {
-    const game = new Canvas('canvas');
-    const p1 = new Player('red', 'p1', 1, 16, 16, 0, 0);
-    const p2 = new Player('purple', 'p2', 2, 16, 16, 100, 100);
+    public canvas: Canvas;
+    public running: boolean;
 
-    game.drawPlayer(p1);
-    game.drawPlayer(p2);
+    constructor() {
+        window.addEventListener('load', () => this.load());
+    }
+
+    private load(): void {
+        this.running = false;
+        this.canvas = new Canvas('canvas');
+        const p1 = new Player('red', 'p1', 1, 16, 16, 0, 0);
+        const p2 = new Player('purple', 'p2', 2, 16, 16, 100, 100);
+    
+        this.canvas.drawPlayer(p1);
+        this.canvas.drawPlayer(p2);
+    }
+
+    public start(): void {
+    }
+
+    public update(): void {
+    }
+
+    public stop(): void {
+    }
+
+    public render(): void {
+    }
+
+    public destroy(): void {
+        window.removeEventListener('load', () => this.load());
+    }
 }
+new Game();
